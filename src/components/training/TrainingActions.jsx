d2ed65246aaae407198f5869cb63dc9f2a3e54a3
Dustin Herboldshimer and Todd Aden <dustin+todd@>
2019-07-11 12:51:46 -0700

    commit for push

--- src/components/training/TrainingActions.jsx
+++ src/components/training/TrainingActions.jsx
@@ -15,3 +15,20 @@ export default function TrainingActions(props) {
         </div>
     )
 }
+
+
+//Stun
+//roll of 20 or higher gets a crit
+
+
+//dex Change to evade and crit
+
+//Strength Damage
+//Accuracy Hit Chance/crit chance
+//Speed move speeds
+//Vitality Max health
+
+
+
+//d6
+//d10
