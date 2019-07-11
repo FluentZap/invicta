13e831e0f8b42e190c62a1c137edbb1d11463ac8
Dustin Herboldshimer <dustnpdx@gmail.com>
2019-07-11 13:22:51 -0700

    create CombatAvatar, CombatView and CombatActions, create links between combat and training views

---
+++ src/components/combat/CombatAvatar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CombatAvatar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CombatAvatar
--- src/components/combat/CombatView.jsx
+++ src/components/combat/CombatView.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import {Link} from '@reach/router'
 
 export default function CombatView() {
     return (
         <div>
-            
+            <h1>Combat View</h1>
+            <Link to='/'>TrainingView</Link>
         </div>
     )
 }
